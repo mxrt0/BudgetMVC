1cbd31dc8377d667702b5c9aae82f43e641b0fe2
Martin Terziev <m.terziev08@abv.bg>
2025-10-18 08:58:53 +0300

    Add project files.

---
+++ BudgetMVC.Data/BudgetDbContext.cs
@@ -0,0 +1,31 @@
+﻿using BudgetMVC.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetMVC.Data;
+
+public class BudgetDbContext : DbContext
+{
+    public BudgetDbContext()
+    {
+
+    }
+
+    public BudgetDbContext(DbContextOptions<BudgetDbContext> options) : base(options)
+    {
+
+    }
+
+    DbSet<Transaction> Transactions { get; set; }
+
+    DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Transaction>()
+            .HasOne(t => t.Category)
+            .WithMany(c => c.Transactions)
+            .HasForeignKey(t => t.CategoryId);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ BudgetMVC.Data/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿namespace BudgetMVC.Data.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+
+    public string? Description { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+}
+
+
